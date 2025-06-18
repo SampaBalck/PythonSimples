b3e9e020f13cd01700bf6f74d87cf11d9168e6c7
SampaBalck <marcelospibm@gmail.com>
2025-06-18 13:39:10 -0300

    Envio do projeto

---
+++ exemplo.py
@@ -0,0 +1,45 @@
+print("Hello World")
+idade = 25
+
+x = 10
+print(idade, x) 
+
+#### python.exe -m pip install --upgrade pip ####
+
+#### pip freeze ####   pega nomes de arquivos requirements.txt
+#### pip freeze > requirements.txt ####  atualiza txt
+#### pip install -r .\requirements.txt ####  instala tudo 
+
+#### pip show dash ####
+
+### Set-ExecutionPolicy RemoteSigned ###
+#### python -m venv .venv ####
+#### .\.venv\Scripts\activate ####
+#### Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass ####
+
+#### Se erro ao ativar venv ####
+### Set-ExecutionPolicy RemoteSigned ###
+
+#### .\.venv\Scripts\Activate.ps1 ####
+
+
+#### pip list ####
+#### python.exe -m pip install --upgrade pip ####
+
+#### pip install numpy pandas pytz six tzdata matplotlib seaborn ####
+
+
+#### ativar GIT ####
+
+#### git init ####
+#### git status ####
+
+### ativar staginh co git ####
+
+### git add . ###
+### git commit -m "comentario" ###
+### git push ###
+### git pull ###
+### git checkout ###
+
+### git add .gitignore ###
